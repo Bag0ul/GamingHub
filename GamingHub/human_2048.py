4ad362074d3c6b0b196a4ed7ac4611a035224130
Bag0ul <45032269+Bag0ul@users.noreply.github.com>
2018-11-22 22:14:30 +0100

    Graphic module

--- GamingHub/human_2048.py
+++ GamingHub/human_2048.py
@@ -1,18 +1,32 @@
-def set_direction():
-    move= ""
-    while not move in ["g","d","h","b"]:
-        move = input("Entrez votre commande (g (gauche), d (droite), h (haut), b (bas)):")
-    return move
+# ----- Récupérer les entrées
+def set_theme_grid():
+    global Entry_theme
+    theme = ""
+    i = 0
+    while not theme in ["0", "1", "2"]:
+
+        theme = Entry_theme.get()
+        if i > 0:
+            modif_commentaire("le théme doit être 0,1 ou 2")
+        i += 1
+    return int(theme)
+
 
 def set_size_grid():
-    size=""
+    global Entry_size
+    size = ""
+    i = 0
     while not size.isdigit():
-        size = input("Entrez la taille de la grille")
+
+        size = Entry_size.get()
+        if i > 0:
+            modif_commentaire("Entrer un entier svp")
+
+        i += 1
     return int(size)
 
-def set_theme_grid():
-    theme=""
-    while not theme in ["0","1","2"]:
-        theme = input ("Entrez le numero du theme")
-    return theme
-
+def set_direction():
+    global command
+    waiting()
+    print(command.get())
+    return (command.get())
