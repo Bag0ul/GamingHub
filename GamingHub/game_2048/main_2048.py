6008e946be2486ee19f9e65980cfe01a1ab3a955
Bag0ul <45032269+Bag0ul@users.noreply.github.com>
2018-11-23 14:08:36 +0100

    Add files via upload

--- GamingHub/game_2048/main_2048.py
+++ GamingHub/game_2048/main_2048.py
@@ -3,8 +3,6 @@ import move_2048
 import ai_2048
 import display_2048
 
-
-
 def play():
     ai = display_2048.set_ai_choice()
     dic_button={'Up':((4,1),'h'),'Down':((5,1),'b'),'Right':((5,2),'d'),'Left':((5,0),'g')}
@@ -13,24 +11,24 @@ def play():
     size = display_2048.set_size_grid()
     grid = grid_2048.init_game(size)
     dic_values = {"round":1}
-    display_2048.display_grid(grid, root)
+    display_2048.display_grid(grid, display_2048.root)
     while not move_2048.is_game_over(grid) and grid_2048.get_grid_tile_max(grid) < 2048:
         moves = move_2048.move_possible(grid)
         dic_values["moves"] = [dic_move[i] for i in range(4) if moves[i]]
         if ai:
             command = ai_2048.set_direction(grid, dic_values)
         else:
-            command = display.set_direction()
+            command = display_2048.set_direction()
         while not command in dic_values["moves"]:
             if ai:
                 command = ai_2048.set_direction(grid, dic_values)
             else:
-                command = display.set_direction()
+                command = display_2048.set_direction()
         grid = move_2048.move_grid(grid, command)
         if not grid_2048.is_full_grid(grid):
             grid = grid_2048.grid_add_new_tile(grid)
         print("Tour {}, deplacement {} :".format(dic_values["round"], command))
-        display_2048.display_grid(grid, root)
+        display_2048.display_grid(grid, display_2048.root)
         print(grid_2048.grid_to_string(grid))
         dic_values["round"] += 1
     if grid_2048.get_grid_tile_max(grid) >= 2048:
